f5e18622d6497fa1e2c76bb26e392e0c9ad0553d
EHERMIN <ehermin@alumno.upv.es@ldsic-vdi06.upvnet.upv.es>
2023-10-18 19:01:00 +0200

    Añadido busqueda binaria inversa para vectores descendentes

--- DyV.h
+++ DyV.h
@@ -19,6 +19,23 @@ T BusquedaBinaria(T x, vector<T>& v, int ini, int fin){
 	return BusquedaBinaria(x, v, m+1, fin);
 
   }  
+}
 
+template<typename T>
+T BusquedaBinaria_INV(T x, vector<T>& v, int ini, int fin){
+  if(ini >fin){
+        return -1;
+  }
+  int m = (ini + fin) / 2;
+  if(v[m] == x){
+        return m;
+  }
+  else if(v[m] > x){
+        return BusquedaBinaria(x, v, m+1, fin);
+  }
+  else{
+        return BusquedaBinaria(x, v, ini, m-1);
 
-}	
+  }
+}
+
--- test_DyV.cpp
+++ test_DyV.cpp
@@ -6,6 +6,8 @@ using namespace std;
 
 int main(){
    vector<int> v{2, 3, 4, 6, 7, 9};
+   vector<int> v2{9, 8, 7, 4, 2, 1};
+
    int sol = BusquedaBinaria(4, v, 0, v.size());
    if(sol == -1){
 	cout << "No se encuentra en el array" << endl;
@@ -13,6 +15,14 @@ int main(){
    else{
 	cout << "Está en la posición: " << sol << endl;
    }   
+   int sol2 = BusquedaBinaria_INV(4, v2, 0, v2.size());
+   if(sol2 == -1){
+        cout << "No se encuentra en el array" << endl;
+   }
+   else{
+        cout << "Está en la posición: " << sol2 << endl;
+   }
+
 
 
 
