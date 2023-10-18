055fccb6374ad4c0a3d209ec8e006c718a284bcd
EHERMIN <ehermin@alumno.upv.es@linux-vdi01.upvnet.upv.es>
2023-10-19 00:26:37 +0200

    Realizado quicksort

--- DyV.h
+++ DyV.h
@@ -3,7 +3,6 @@
 using namespace std;
 
 template<typename T>
-
 T BusquedaBinaria(T x, vector<T>& v, int ini, int fin){
   if(ini >fin){
 	return -1;
@@ -17,7 +16,6 @@ T BusquedaBinaria(T x, vector<T>& v, int ini, int fin){
   }
   else{
 	return BusquedaBinaria(x, v, m+1, fin);
-
   }  
 }
 
@@ -35,7 +33,42 @@ T BusquedaBinaria_INV(T x, vector<T>& v, int ini, int fin){
   }
   else{
         return BusquedaBinaria(x, v, ini, m-1);
-
   }
 }
 
+template<typename T>
+int Partition(vector<T>& v, int ini, int fin){
+   T x = v[fin];
+   int i = ini;
+   for(int j= ini; j< fin; j++){
+	if(v[j] <= x){
+	   T temp = v[i];
+	   v[i] = v[j];
+	   v[j] = temp;
+	   i++;
+	}
+   }
+   T temp2 = v[i];
+   v[i] = v[fin];
+   v[fin] = temp2;
+   return i;
+}
+
+template<typename T>
+T QuickSort(vector<T>& v, int ini, int fin){
+   if(ini<fin){
+        int p = Partition(v, ini, fin);
+        QuickSort(v, ini, p-1);
+        QuickSort(v, p+1, fin);
+   }
+}
+
+
+template<typename T>
+void printArr(vector<T>& v, int fin){
+   cout << "[";
+   for(int i=0; i<fin; i++){
+	cout << v[i] << ", ";
+   }
+   cout << "] " << endl;
+}
--- test_DyV.cpp
+++ test_DyV.cpp
@@ -7,14 +7,19 @@ using namespace std;
 int main(){
    vector<int> v{2, 3, 4, 6, 7, 9};
    vector<int> v2{9, 8, 7, 4, 2, 1};
-
+   
+   cout << "Búsqueda binaria: " << endl;
+   printArr(v, v.size());   
    int sol = BusquedaBinaria(4, v, 0, v.size());
    if(sol == -1){
 	cout << "No se encuentra en el array" << endl;
    }
    else{
 	cout << "Está en la posición: " << sol << endl;
-   }   
+   } 
+   
+   cout << "Búsqueda binaria invertida: " << endl;
+   printArr(v2, v2.size());
    int sol2 = BusquedaBinaria_INV(4, v2, 0, v2.size());
    if(sol2 == -1){
         cout << "No se encuentra en el array" << endl;
@@ -23,6 +28,11 @@ int main(){
         cout << "Está en la posición: " << sol2 << endl;
    }
 
+   cout << "QuickSort: " << endl;
+   printArr(v2, v2.size());
+   QuickSort(v2, 0, v2.size());
+   printArr(v2, v2.size());
+ 
 
 
 
